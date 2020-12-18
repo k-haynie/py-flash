57e7b7764388a5a4526fd33b6ee44710a73cb90c
K-Haynie <76216676+K-Haynie@users.noreply.github.com>
2020-12-18 10:08:56 -0500

    Pieces of a flashcards app

---
+++ import.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+import shutil
+
+
+base = Tk() 
+
+base.geometry('300x300')
+
+def file_opener():
+	#Gets directory (filedir)
+	filedir = filedialog.askopenfilename(initialdir="/")
+	print(filedir)
+
+	#creates a savelocation directory
+	savelocation = 'Decks'
+	print(savelocation) 
+
+
+	#establishes a Decks file for the csv file
+	try:
+		os.makedirs(savelocation)    
+		print("Directory ", savelocation,  " Created ")
+	except FileExistsError:
+		print("Directory ", savelocation,  " already exists")
+
+	finality = os.path.abspath(savelocation)
+	print(finality)
+
+
+	shutil.copy(filedir, finality)
+	print("success")
+
+
+button = Button(base, text ='Select a .txt/.csv file', command = lambda:file_opener())
+button.pack()
+mainloop()
+
+#figure out a way to break out of the import window. Maybe switch to a tkinter alternative?
